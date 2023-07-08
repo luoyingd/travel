754aba15d304ee4c0485afb6f9beaf1c8e6974bc
luoyingd <luoyingd@student.unimelb.edu.au>
2023-07-08 23:01:20 +0800

    fix: send photo

--- backend/Work/note/SubscribeReceiver.cs
+++ backend/Work/note/SubscribeReceiver.cs
@@ -8,14 +8,15 @@ namespace backend.Work.Note
         {
             SubscribePublisher publisher = (SubscribePublisher)sender;
             SubscribeWork work = (SubscribeWork)e;
-            // TODO: send mail
             Console.WriteLine(@$"{_email}收到{publisher.SenderEmail}的新note, 新的note是{work.NewNote.Title}, 
             {work.NewNote.Id}, {work.NewNote.Address}, {work.NewNote.Photos}");
             // send mail
             Dictionary<string, string> parameters = new(){
                 {"username", "Hi " + work.NewNote.FirstName + ","},
                 {"my-title",work.NewNote.Title},
-                {"src", Constant.Constant.URL + "/common/photo/" + Constant.Constant.BLANK_KEY},
+                {"src", work.NewNote.Photos != null && work.NewNote.Photos.Length > 0
+                ? Constant.Constant.URL + "/common/photo/" + work.NewNote.Photos.Split(",")[0]
+                : Constant.Constant.URL + "/common/photo/" + Constant.Constant.BLANK_KEY},
                 {"href", Constant.Constant.INFO_HREF + work.NewNote.Id}
             };
             _mailUtil.sendMail(Constant.Constant.BASE_DIR + "/send_subscription.html", _email,
